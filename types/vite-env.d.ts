de1d03baa209bd4d4b6384646a7a2987dff2dd5f
Mohan <vkmohan112@gmail.com>
2025-12-17 13:41:51 +0530

    Fix Render build: move build-time deps to dependencies

--- types/vite-env.d.ts
+++ types/vite-env.d.ts
@@ -1,6 +1,19 @@
-/// <reference types="vite/client" />
-
 interface ImportMetaEnv {
   readonly VITE_API_URL?: string
   readonly VITE_GOOGLE_CLIENT_ID?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare module 'vite/client' {
+  interface ImportMetaEnv {
+    readonly VITE_API_URL?: string
+    readonly VITE_GOOGLE_CLIENT_ID?: string
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
 }
