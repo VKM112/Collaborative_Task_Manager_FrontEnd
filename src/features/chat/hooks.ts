935d97a40ce3064f259d9b4c05ffe27d88d97aa9
Mohan <vkmohan112@gmail.com>
2025-12-18 16:29:09 +0530

    fixed small bug in hooks.ts

--- src/features/chat/hooks.ts
+++ src/features/chat/hooks.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { createTeamMessage, getTeamMessages } from '../../api/messages.api'
 import type { TeamMessage } from './types'
 
